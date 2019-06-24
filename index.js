b5d68e047b305bd3ba410e6c2fb5f64e8af53847
Brenden Palmer <brendenpalmer@gmail.com>
2019-06-23 22:54:04 -0700

    Remove dependency on `lodash.curryright`

--- index.js
+++ index.js
@@ -2,13 +2,4 @@
 
 module.exports = {
   name: require('./package').name,
-  importTransforms: require('ember-cli-cjs-transform').importTransforms,
-
-  included() {
-    this._super.included.apply(this, arguments);
-
-    this.import('node_modules/lodash.curryright/index.js', {
-      using: [{ transformation: 'cjs', as: 'lodash/curryRight' }],
-    });
-  },
 };
---
+++ tests/unit/-internal/curry-utils-test.js
@@ -0,0 +1,29 @@
+import { module, test } from 'ember-qunit';
+import { curryRight } from 'ember-exception-handling/-internal/curry-utils';
+
+module('Unit | Utility | exception-handling/-internal/curry-utils', function() {
+  test('curryRight should work as expected', function(assert) {
+    assert.expect(8);
+
+    const add = (a, b, c) => {
+      return a + b + c;
+    };
+
+    const subtract = (a, b, c) => {
+      return c - b - a;
+    };
+
+    const curriedAdd = curryRight(add);
+    const curriedSubtract = curryRight(subtract);
+
+    assert.equal(add(1, 2, 3), curriedAdd(3)(2)(1));
+    assert.equal(curriedAdd(3)(2)(1), 6);
+    assert.equal(curriedAdd(3)(1, 2), 6);
+    assert.equal(curriedAdd(1, 2, 3), 6);
+
+    assert.equal(subtract(1, 2, 6), curriedSubtract(6)(2)(1));
+    assert.equal(curriedSubtract(6)(2)(1), 3);
+    assert.equal(curriedSubtract(1, 2, 6), 3);
+    assert.equal(curriedSubtract(6)(1, 2), 3);
+  });
+});
